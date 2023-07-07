2a08635ea2a7c59253081318c52b0acc990b26ea
Zach Chairez <89652451+zach-chairez@users.noreply.github.com>
2023-07-07 16:23:21 -0500

    Update change_bases_to_int.py

--- python_functions/change_bases_to_int.py
+++ python_functions/change_bases_to_int.py
@@ -4,5 +4,5 @@
 def change_bases_to_int(sequence):
   seq_new = []; gene_mapping = {'A': 0, 'C': 1, 'T': 2, 'G': 3,'a':0,'c':1,'t':2,'g':3}
   for seq in sequence:
-    seq_new.append([gene_mapping[base] if base in gene_mapping else -1 for base in sequence]
-    return seq_new
+    seq_new.append([gene_mapping[base] if base in gene_mapping else -1 for base in seq]
+  return seq_new
